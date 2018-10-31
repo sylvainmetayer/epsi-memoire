7de2c32c7d3659d2bd544306693096c45a748dd8
Sylvain METAYER <contact@sylvainmetayer.fr>
2018-10-31 21:35:32 +0100

    build(travis): test

--- .travisci/texlive_install.sh
+++ .travisci/texlive_install.sh
@@ -16,7 +16,7 @@ if ! command -v texlua > /dev/null; then
   cd install-tl-20*
 
   # Find directory this file is in, to find the texlive.profile file.
-  BASEDIR=$( cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P )
+  BASEDIR=$( cd "$(dirname ${BASH_SOURCE[0]})"; pwd -P )
 
   # Install a minimal system
   ./install-tl --profile="$BASEDIR"/texlive.profile
